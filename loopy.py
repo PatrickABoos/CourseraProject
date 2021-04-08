b3366bc0acdd374f9d65b58ec13726fb3c2528aa
PatrickABoos <82179648+PatrickABoos@users.noreply.github.com>
2021-04-08 14:55:33 -0500

    Add files via upload
    Python Loop Example.

---
+++ loopy.py
@@ -0,0 +1,9 @@
+assholes = ['chad', 'mikeboos', 'prentice_cannotbelievesomeonewouldnamehimthat','jasonhainesfuckboston_oknotreallyfuckboston']
+for assholes in assholes:
+	print(f"{assholes.title()}, << this guy is an asshole!!!")
+	print(f"I can't wait until Bubba pumps your ass!")
+	print(f"{assholes.title()} fuck off")
+print(f"I hate all of you.")
+
+
+
