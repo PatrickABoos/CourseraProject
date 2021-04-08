173058c5b35493aa80a97a331ee6f876426bf52e
PatrickABoos <82179648+PatrickABoos@users.noreply.github.com>
2021-04-08 14:56:41 -0500

    Add files via upload

---
+++ LoopySlice.py
@@ -0,0 +1,4 @@
+players = ['john_holmes','lex_steele','peter_north','dingle_dan','allen_locust']
+print("These guys are the first playas on my team:")
+for player in players[:3]:
+	print(player.title())
