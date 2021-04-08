45a38ea59fd1452502d0a639660b470f67071497
PatrickABoos <82179648+PatrickABoos@users.noreply.github.com>
2021-04-08 14:56:30 -0500

    Delete loopy.py

--- loopy.py
+++
@@ -1,9 +0,0 @@
-assholes = ['chad', 'mikeboos', 'prentice_cannotbelievesomeonewouldnamehimthat','jasonhainesfuckboston_oknotreallyfuckboston']
-for assholes in assholes:
-	print(f"{assholes.title()}, << this guy is an asshole!!!")
-	print(f"I can't wait until Bubba pumps your ass!")
-	print(f"{assholes.title()} fuck off")
-print(f"I hate all of you.")
-
-
-
