42db276bf0d158a0b86f22452c833d68e4cb5069
PatrickABoos <82179648+PatrickABoos@users.noreply.github.com>
2021-04-08 14:56:56 -0500

    Delete LoopySlice.py

--- LoopySlice.py
+++
@@ -1,4 +0,0 @@
-players = ['john_holmes','lex_steele','peter_north','dingle_dan','allen_locust']
-print("These guys are the first playas on my team:")
-for player in players[:3]:
-	print(player.title())
