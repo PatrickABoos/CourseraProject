f25778572ba88a50205b7c7d9290e36304e24caa
PatrickABoos <82179648+PatrickABoos@users.noreply.github.com>
2021-04-08 14:57:13 -0500

    Add files via upload

---
+++ foods.py
@@ -0,0 +1,10 @@
+my_foods = ['burnt_ends','ribs','cheese_cake','broccoli','green_beans']
+friend_foods = my_foods[2:3]
+my_foods.append('spotted_dick')
+friend_foods.append('bangers_and_mash')
+print('My favorite foods are:')
+print(my_foods)
+print('\nBilly Bob likes:')
+print(friend_foods)
+ladies_no_like_food = my_foods[-1:]
+print(f'The ladies try to stay away from the {ladies_no_like_food}.\n')
